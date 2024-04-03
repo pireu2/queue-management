da4fc40646a686ae1878b725f8c83dd6d57e6e9d
Duica Sebastian <sebi.duica@gmail.com>
2024-04-03 16:08:13 +0300

    added logger

--- src/main/java/org/example/logic/SimulationManager.java
+++ src/main/java/org/example/logic/SimulationManager.java
@@ -5,6 +5,9 @@ import org.example.model.Server;
 import org.example.model.Task;
 
 import javax.swing.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -19,6 +22,7 @@ public class SimulationManager  implements Runnable{
     private StrategyPolicy strategyPolicy;
     private Scheduler scheduler;
     private ArrayList<Task> generatedTasks;
+    private Logger logger;
 
     public SimulationManager(SimulationView view, int timeLimit, int maxProcessingTime, int minProcessingTime, int numberOfServers, int numberOfClients, StrategyPolicy strategyPolicy){
         this.timeLimit = timeLimit;
@@ -34,6 +38,7 @@ public class SimulationManager  implements Runnable{
         for(int i = 0; i < this.numberOfServers; i++){
             scheduler.addServer();
         }
+        logger = new Logger();
     }
 
     public ArrayList<Task> getGeneratedTasks(){
@@ -44,13 +49,31 @@ public class SimulationManager  implements Runnable{
     }
 
     public void run(){
+        try {
+            logger.writer = new BufferedWriter(new FileWriter("log.txt")); // Overwrite the existing content of the log file
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         currentTime = 0;
         while(currentTime <= timeLimit){
+            logger.log(
+                    "Time: " + currentTime
+            );
+            for(Server server : scheduler.getServers()){
+                logger.log(
+                        "\tServer " + scheduler.getServers().indexOf(server) + " has " + server.getNoTasks() + " tasks and a waiting period of " + server.getWaitingPeriod()
+                );
+                for(Task task : server.getTasks()){
+                    logger.log(
+                            "\t\tTask " + task.getId() + " has " + task.getServiceTime() + " remaining service time and arrived at " + task.getArrivalTime()
+                    );;
+                }
+            }
             view.update();
             Iterator<Task> taskIterator = generatedTasks.iterator();
             while(taskIterator.hasNext()){
                 Task task = taskIterator.next();
-                if(task.getArrivalTime() == currentTime){
+                if(task.getArrivalTime() == currentTime + 1){
                     taskIterator.remove();
                     scheduler.dispatchTask(task);
                 }
@@ -75,6 +98,28 @@ public class SimulationManager  implements Runnable{
             int serviceTime = (int)(Math.random() * (maxProcessingTime - minProcessingTime) + minProcessingTime);
             Task task = new Task(i, arrivalTime, serviceTime);
             generatedTasks.add(task);
+        }
+    }
+
+    private class Logger{
+        private BufferedWriter writer;
+
+        public Logger(){
+            try {
+                writer = new BufferedWriter(new FileWriter("log.txt", true));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void log(String message){
+            try {
+                writer.write(message);
+                writer.newLine();
+                writer.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
